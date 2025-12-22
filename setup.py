d0f5d00526efb798b68023d9923b01c3b9a6e1c6
sarthak <sarthakhirpurkarmh27@gmail.com>
2025-12-22 21:00:02 +0530

    Requirement added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Sarthak Hirpurkar",
+    author_email="sarthakhirpurkarmh27@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
